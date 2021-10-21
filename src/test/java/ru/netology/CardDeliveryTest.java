e63a6787b6e8ec26ae9a4f57d07656b1b56542d5
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-21 12:13:01 +0300

    Changes files

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,11 +1,21 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardDeliveryTest {
+
+    @BeforeEach
+    void setUp() {
+
+        Configuration.browser = "chrome";
+        open("http://localhost:9999");
+    }
 
 
 }
--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,4 +1,32 @@
 package ru.netology;
 
+import com.github.javafaker.Faker;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.val;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
 public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        String dateOfMeeting = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return dateOfMeeting;
+    }
+
+
+
 }
+
+
+
+
+
+
+
