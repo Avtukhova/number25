6805e7a2c76f6fcd1f45247d3937e576f9ad7353
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-21 10:40:42 +0300

    Changes file

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,2 +1,18 @@
+
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+
+
 public class CardDeliveryTest {
+
+    @Data
+    @RequiredArgsConstructor
+
+    public class RegistrationInfo {
+        private final String city;
+        private final String name;
+        private final String phone;
+    }
 }
